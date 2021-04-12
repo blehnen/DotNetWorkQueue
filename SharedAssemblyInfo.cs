01f4199c17ac9b570417a51ea6f0be0dc42d8493
Brian Lehnen <brian.lehnen74@gmail.com>
2021-04-12 14:51:00 -0500

    1) Restore SharedAssemblyInfo.cs as the examples still use it.

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Brian Lehnen")]
+[assembly: AssemblyCopyright("Copyright © Brian Lehnen 2021")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.5.1")]
+[assembly: AssemblyFileVersion("0.5.1")]
+[assembly: AssemblyInformationalVersion("0.5.1")] 
