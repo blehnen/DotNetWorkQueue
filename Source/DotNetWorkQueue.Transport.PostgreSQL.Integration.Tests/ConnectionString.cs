130c2df5f34c094241b369590a89a7e0da329432
blehnen <brian.lehnen74@gmail.com>
2024-01-15 21:53:19 -0600

    Update integration test connection string for NPGSQL 8

--- Source/DotNetWorkQueue.Transport.PostgreSQL.Integration.Tests/ConnectionString.cs
+++ Source/DotNetWorkQueue.Transport.PostgreSQL.Integration.Tests/ConnectionString.cs
@@ -6,6 +6,6 @@
         /// The connection string to the SQL DB for the integration tests. All tests in this project will use this connection string
         /// </summary>
         public static string ConnectionString =
-            "Server=192.168.0.58;Port=5432;Database=integrationtesting;Integrated Security=true;Maximum Pool Size=250;userid=brian";
+            "Server=192.168.0.58;Port=5432;Database=integrationtesting;Maximum Pool Size=250;userid=brian;Trust Server Certificate=true";
     }
 }
