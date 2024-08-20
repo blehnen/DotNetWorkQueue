608a77940399cc4f7beba814b59212739441372e
blehnen <brian.lehnen74@gmail.com>
2024-08-20 10:19:28 -0500

    Update package dependencies

--- Source/DotNetWorkQueue.Transport.PostgreSQL.Integration.Tests/ConnectionString.cs
+++ Source/DotNetWorkQueue.Transport.PostgreSQL.Integration.Tests/ConnectionString.cs
@@ -6,6 +6,6 @@
         /// The connection string to the SQL DB for the integration tests. All tests in this project will use this connection string
         /// </summary>
         public static string ConnectionString =
-            "Server=192.168.0.58;Port=5432;Database=integrationtesting;Maximum Pool Size=250;userid=brian;Trust Server Certificate=true";
+            "Server=192.168.0.2;Port=5432;Database=integrationtesting;Maximum Pool Size=250;userid=brian;Trust Server Certificate=true";
     }
 }
