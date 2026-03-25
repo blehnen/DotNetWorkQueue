c7a1d77b71bfbfc806b78356aa7d8bd7847671b8
blehnen <brian.lehnen74@gmail.com>
2026-03-25 09:03:25 -0500

    Histoy pagenation

--- Source/DotNetWorkQueue.Transport.SQLite/Basic/SqLiteMessageQueueInit.cs
+++ Source/DotNetWorkQueue.Transport.SQLite/Basic/SqLiteMessageQueueInit.cs
@@ -50,6 +50,7 @@ namespace DotNetWorkQueue.Transport.SQLite.Basic
             container.Register<IWriteMessageHistory, WriteMessageHistoryHandler>(LifeStyles.Singleton);
             container.Register<IQueryMessageHistory, QueryMessageHistoryHandler>(LifeStyles.Singleton);
             container.Register<IPurgeMessageHistory, PurgeMessageHistoryHandler>(LifeStyles.Singleton);
+            container.Register<IDbPaginationSyntax, LimitOffsetPaginationSyntax>(LifeStyles.Singleton);
 
             container.Register<IBaseTransportOptions>(() =>
             {
